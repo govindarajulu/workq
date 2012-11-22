c8fa957cceae7bc93cff855fc984035726c40c77
govind <govindarajulu90@gmail.com>
2012-11-22 15:06:17 +0530

    adding tasklet

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/interrupt.h>
+#include <linux/delay.h>
+
+void my_tasklet(unsigned long data)
+{
+	while(1) {
+		printk(KERN_INFO "running tasklet\n");
+		udelay(10000);
+	}
+
+}
 
 static __init int modinit(void)
 {
+	DECLARE_TASKLET(mytask, my_tasklet, 10);
 	printk(KERN_INFO"Hello World!\n");
 	return 0;
 }
