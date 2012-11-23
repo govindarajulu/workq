2426bc123131b39094c7df3390a7b1ce3460f5e3
govind <govindarajulu90@gmail.com>
2012-11-23 10:50:01 +0530

    a working example of tasklet

--- main.c
+++ main.c
@@ -7,18 +7,17 @@ void my_tasklet(unsigned long data)
 {
 	int i=100;
 	while(i > 0) {
-		printk(KERN_INFO "running tasklet\n");
-		udelay(100000);
+		printk(KERN_INFO "running tasklet=%d\n",i);
+		udelay(1000);
 		i--;
 	}
-		return;
+	return;
 
 }
 unsigned long int data=100;
 DECLARE_TASKLET(mytask, my_tasklet, &data);
 static __init int modinit(void)
 {
-
 	printk(KERN_INFO"Hello World!\n");
 	//tasklet_init(&mytask,my_tasklet, &data);
 	//tasklet_enable(&mytask);
@@ -28,7 +27,6 @@ static __init int modinit(void)
 
 static __exit void modexit(void)
 {
-
 	tasklet_kill(&mytask);
 }
 
