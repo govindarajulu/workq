adb6c3074027bd3fd0a06373b1dc71d778a1bfd4
govind <govindarajulu90@gmail.com>
2012-11-23 11:19:33 +0530

    workq

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <linux/kernel.h>
 #include <linux/interrupt.h>
 #include <linux/delay.h>
+#include <linux/workqueue.h>
 
 void my_tasklet(unsigned long data)
 {
@@ -14,19 +15,35 @@ void my_tasklet(unsigned long data)
 	return;
 
 }
+void my_wq_fun(void* i)
+{
+	printk(KERN_INFO "this is my wq function\n");
+}
+
 unsigned long int data=100;
 DECLARE_TASKLET(mytask, my_tasklet, &data);
+
+struct workqueue_struct *my_wq;
+struct work_struct *my_work_struct;
 static __init int modinit(void)
 {
 	printk(KERN_INFO"Hello World!\n");
-	//tasklet_init(&mytask,my_tasklet, &data);
-	//tasklet_enable(&mytask);
+	my_wq = create_workqueue("goDgo's queue");
+	if(!my_wq)
+		return -1;
+	my_work_struct = kmalloc(sizeof(struct work_struct),GFP_KERNEL);
+	INIT_WORK(my_work_struct,my_wq_fun);
+	queue_work(my_wq,my_work_struct);
+
+
 	tasklet_schedule(&mytask);
 	return 0;
 }
 
 static __exit void modexit(void)
 {
+	flush_workqueue(my_wq);
+	destroy_workqueue(my_wq);
 	tasklet_kill(&mytask);
 }
 
